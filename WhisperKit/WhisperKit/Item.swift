8599ddf6d1accfa18ff6f977d58702ff032dff52
Matthew Pazaryna <52605785+mpazaryna@users.noreply.github.com>
2025-11-25 13:57:01 -0500

    first commit

---
+++ WhisperKit/WhisperKit/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  WhisperKit
+//
+//  Created by MATTHEW PAZARYNA on 11/25/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ WhisperKit/WhisperKitTests/WhisperKitTests.swift
@@ -0,0 +1,16 @@
+//
+//  WhisperKitTests.swift
+//  WhisperKitTests
+//
+//  Created by MATTHEW PAZARYNA on 11/25/25.
+//
+
+import Testing
+
+struct WhisperKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
